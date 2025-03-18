e3136255dbb66b5530031fd8dc32da462c149ce4
Antonio Campolo <antonio.campolo1978@gmail.com>
2025-03-18 16:07:05 +0100

    scaffolding

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+
+<div class="container-fluid p5 bg-secondary-subtle text-center">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <h1 class="display-1">The Aulab Post</h1>
+        </div>
+
+    </div>
+</div>
+
+
+
+</x-layout>
